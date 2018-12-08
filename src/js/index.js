dd6a1e6cfe5fa0ae7838bfc713f3fd15d3787b7b
gillianbc <gszemeti@gmail.com>
2018-12-08 20:41:36 +0000

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+// Global app controller
+import something from './myfile'
+const x = 1112;
+console.log(`The variable is ${x} and ${something}`)
---
+++ src/js/myfile.js
@@ -0,0 +1,2 @@
+console.log("I am myfile.js");
+export default 'Hello from myfile.js'
